be0ab676acfc1db4724bb6e5b3ae433f0f320ae6
prathamesh <prathamesh69bhavsar@gmail.com>
2025-07-23 17:42:03 +0530

    added journey page

--- src/components/public/home/homeAds/HomeAds.jsx
+++ src/components/public/home/homeAds/HomeAds.jsx
@@ -6,8 +6,12 @@ import news1 from '../../../../assets/images/news1.png'
 import news3 from '../../../../assets/images/news3.png'
 import news4 from '../../../../assets/images/news4.png'
 import news5 from '../../../../assets/images/news5.png'
+import news6 from '../../../../assets/images/news6.png'
+import news7 from '../../../../assets/images/news7.png'
+import news8 from '../../../../assets/images/news8.png'
+import news9 from '../../../../assets/images/news9.png'
 
-const newsImages = [news1, news2, news3, news4, news5];
+const newsImages = [news1, news2, news3, news4, news5, news6, news7, news8, news9];
 
 const HomeAds = () => {
     return (
