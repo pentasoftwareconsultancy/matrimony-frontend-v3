71c47cddb97b3675328b32da8435b9b6032513ef
sonu2404 <sonaliekhande12@gmail.com>
2025-07-24 14:24:28 +0530

    routing adding

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PublicFooter from './components/public/publicFooter/PublicFooter'
 import AboutPage from './pages/publicPages/AboutPage'
 import RegisterPage from './pages/publicPages/RegisterPage'
 import Login from './components/public/login/Login'
+import AboutCommittee from './components/public/about/aboutHero/AboutCommittee'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/about" element={<AboutPage />} />
       <Route path="/register" element={<RegisterPage/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/aboutcommittee" element={<AboutCommittee/>}/>
       </Routes>
       <PublicFooter />
     </div>
